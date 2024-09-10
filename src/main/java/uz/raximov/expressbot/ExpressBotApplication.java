f08f1d9373ac701afdd2cb5b45591cb360a1d83f
Rashidbek Raximov <rashidbekraximov21@gmail.com>
2024-09-10 16:02:54 +0500

    initial commit

---
+++ src/main/java/uz/raximov/expressbot/ExpressBotApplication.java
@@ -0,0 +1,13 @@
+package uz.raximov.expressbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpressBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/raximov/expressbot/ExpressBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.raximov.expressbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
